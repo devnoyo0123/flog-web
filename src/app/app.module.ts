ef5373ac28520961ba199377f046194a98006c45
YosephNoh <noyo0123@crocusenergy.com>
2021-10-15 09:35:15 +0900

    setup eslint

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { ko_KR } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import ko from '@angular/common/locales/ko';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+registerLocaleData(ko);
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule, BrowserAnimationsModule],
+  providers: [{ provide: NZ_I18N, useValue: ko_KR }],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
